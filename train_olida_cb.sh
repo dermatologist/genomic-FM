ee3737639beb42ea79109f2536bc6082956fdcae
Bell Eapen <github_public@gulfdoctor.net>
2024-12-10 13:43:48 +0000

    Increase num_records to 15000 in finetune_codonbert.yaml; update training epochs to 10 in training scripts for Codon-Bert and DNA-Bert

--- train_olida_cb.sh
+++ train_olida_cb.sh
@@ -1,1 +1,1 @@
-python finetune.py --dataset='oligogenic_codon_bert' --epochs=2 --gpus=3 --disk_chunk=750 --num_workers=2 --config=configs/finetune_codonbert.yaml --seed=42 --project='Codon-Bert-Olida'
+python finetune.py --dataset='oligogenic_codon_bert' --epochs=10 --gpus=3 --disk_chunk=750 --num_workers=2 --config=configs/finetune_codonbert.yaml --seed=42 --project='Codon-Bert-Olida'
