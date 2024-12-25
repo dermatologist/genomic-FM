62b58bc279d5be53c6d66f25882e52a791d5d4ce
Bell Eapen <github_public@gulfdoctor.net>
2024-12-25 22:16:24 +0000

    Update compare-short.sh to loop through compare-bert-base.py with additional input sizes

--- compare-short.sh
+++ compare-short.sh
@@ -1,7 +1,19 @@
-~/miniconda3/envs/py310/bin/python compare-auroc.py hyena 512
-~/miniconda3/envs/py310/bin/python compare-auroc.py dnab 512
-~/miniconda3/envs/py310/bin/python compare-auroc.py gt 512
-~/miniconda3/envs/py310/bin/python compare-auroc.py hyena 1024
-~/miniconda3/envs/py310/bin/python compare-auroc.py dnab 1024
-~/miniconda3/envs/py310/bin/python compare-auroc.py gt 1024
-
+#!/bin/bash
+while true
+do
+~/miniconda3/envs/py310/bin/python compare-bert-base.py hyena 512
+~/miniconda3/envs/py310/bin/python compare-bert-base.py dnab 512
+~/miniconda3/envs/py310/bin/python compare-bert-base.py gt 512
+~/miniconda3/envs/py310/bin/python compare-bert-base.py hyena 1024
+~/miniconda3/envs/py310/bin/python compare-bert-base.py dnab 1024
+~/miniconda3/envs/py310/bin/python compare-bert-base.py gt 1024
+~/miniconda3/envs/py310/bin/python compare-bert-base.py hyena 1536
+~/miniconda3/envs/py310/bin/python compare-bert-base.py dnab 1536
+~/miniconda3/envs/py310/bin/python compare-bert-base.py gt 1536
+~/miniconda3/envs/py310/bin/python compare-bert-base.py hyena 2048
+~/miniconda3/envs/py310/bin/python compare-bert-base.py dnab 2048
+~/miniconda3/envs/py310/bin/python compare-bert-base.py gt 2048
+~/miniconda3/envs/py310/bin/python compare-bert-base.py hyena 4096
+~/miniconda3/envs/py310/bin/python compare-bert-base.py dnab 4096
+~/miniconda3/envs/py310/bin/python compare-bert-base.py gt 4096
+done
