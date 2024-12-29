a2442e48c622379954ad50b1b8072dba165a2665
Bell Eapen <github_public@gulfdoctor.net>
2024-12-29 21:47:17 +0000

    Update compare-short.sh to use save-datasets.py for data processing

--- compare-short.sh
+++ compare-short.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 while true
 do
-~/miniconda3/envs/py310/bin/python compare-bert-base.py hyena 512
-~/miniconda3/envs/py310/bin/python compare-bert-base.py dnab 512
-~/miniconda3/envs/py310/bin/python compare-bert-base.py gt 512
-~/miniconda3/envs/py310/bin/python compare-bert-base.py hyena 1024
-~/miniconda3/envs/py310/bin/python compare-bert-base.py dnab 1024
-~/miniconda3/envs/py310/bin/python compare-bert-base.py gt 1024
-~/miniconda3/envs/py310/bin/python compare-bert-base.py hyena 1536
-~/miniconda3/envs/py310/bin/python compare-bert-base.py dnab 1536
-~/miniconda3/envs/py310/bin/python compare-bert-base.py gt 1536
-~/miniconda3/envs/py310/bin/python compare-bert-base.py hyena 2048
-~/miniconda3/envs/py310/bin/python compare-bert-base.py dnab 2048
-~/miniconda3/envs/py310/bin/python compare-bert-base.py gt 2048
-~/miniconda3/envs/py310/bin/python compare-bert-base.py hyena 4096
-~/miniconda3/envs/py310/bin/python compare-bert-base.py dnab 4096
-~/miniconda3/envs/py310/bin/python compare-bert-base.py gt 4096
+~/miniconda3/envs/py310/bin/python save-datasets.py hyena 512
+~/miniconda3/envs/py310/bin/python save-datasets.py dnab 512
+~/miniconda3/envs/py310/bin/python save-datasets.py gt 512
+~/miniconda3/envs/py310/bin/python save-datasets.py hyena 1024
+~/miniconda3/envs/py310/bin/python save-datasets.py dnab 1024
+~/miniconda3/envs/py310/bin/python save-datasets.py gt 1024
+# ~/miniconda3/envs/py310/bin/python save-datasets.py hyena 1536
+# ~/miniconda3/envs/py310/bin/python save-datasets.py dnab 1536
+# ~/miniconda3/envs/py310/bin/python save-datasets.py gt 1536
+~/miniconda3/envs/py310/bin/python save-datasets.py hyena 2048
+~/miniconda3/envs/py310/bin/python save-datasets.py dnab 2048
+~/miniconda3/envs/py310/bin/python save-datasets.py gt 2048
+~/miniconda3/envs/py310/bin/python save-datasets.py hyena 4096
+~/miniconda3/envs/py310/bin/python save-datasets.py dnab 4096
+~/miniconda3/envs/py310/bin/python save-datasets.py gt 4096
 done
