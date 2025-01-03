4b315c1dc39fb3e59e11b2c1c81c27a9927dcfb2
Bell Eapen <github@gulfdoctor.net>
2025-01-03 16:58:09 -0600

    Update compare-short.sh to use compare-save-datasets.py for improved script consistency

--- compare-short.sh
+++ compare-short.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 while true
 do
-~/miniconda3/envs/py310/bin/python save-datasets.py hyena 512
-~/miniconda3/envs/py310/bin/python save-datasets.py dnab 512
-~/miniconda3/envs/py310/bin/python save-datasets.py gt 512
-~/miniconda3/envs/py310/bin/python save-datasets.py hyena 1024
-~/miniconda3/envs/py310/bin/python save-datasets.py dnab 1024
-~/miniconda3/envs/py310/bin/python save-datasets.py gt 1024
-# ~/miniconda3/envs/py310/bin/python save-datasets.py hyena 1536
-# ~/miniconda3/envs/py310/bin/python save-datasets.py dnab 1536
-# ~/miniconda3/envs/py310/bin/python save-datasets.py gt 1536
-~/miniconda3/envs/py310/bin/python save-datasets.py hyena 2048
-~/miniconda3/envs/py310/bin/python save-datasets.py dnab 2048
-~/miniconda3/envs/py310/bin/python save-datasets.py gt 2048
-~/miniconda3/envs/py310/bin/python save-datasets.py hyena 4096
-~/miniconda3/envs/py310/bin/python save-datasets.py dnab 4096
-~/miniconda3/envs/py310/bin/python save-datasets.py gt 4096
+python compare-save-datasets.py hyena 512
+python compare-save-datasets.py dnab 512
+python compare-save-datasets.py gt 512
+python compare-save-datasets.py hyena 1024
+python compare-save-datasets.py dnab 1024
+python compare-save-datasets.py gt 1024
+# python compare-save-datasets.py hyena 1536
+# python compare-save-datasets.py dnab 1536
+# python compare-save-datasets.py gt 1536
+python compare-save-datasets.py hyena 2048
+python compare-save-datasets.py dnab 2048
+python compare-save-datasets.py gt 2048
+python compare-save-datasets.py hyena 4096
+python compare-save-datasets.py dnab 4096
+python compare-save-datasets.py gt 4096
 done
